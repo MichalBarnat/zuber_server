8ec2269e697e22c2491f6915bcf0ea0fe27fd8bc
Michał Barnat <michalbarnat@gmail.com>
2023-12-04 09:51:14 +0100

    google matrix distance tested by controller

--- zuber/src/main/java/com/bbc/zuber/test/ControllerTest.java
+++ zuber/src/main/java/com/bbc/zuber/test/ControllerTest.java
@@ -17,9 +17,27 @@ public class ControllerTest {
         this.service = service;
     }
 
-    @PostMapping("/dest")
-    public ResponseEntity<String> test(@RequestBody TestBody body) {
+    @PostMapping("/distance_string")
+    public ResponseEntity<String> distanceString(@RequestBody TestBody body) {
         String distance = service.getDistanceString(body.getFrom(), body.getTo());
         return ResponseEntity.ok(distance);
+    }
+
+    @PostMapping("/duration_string")
+    public ResponseEntity<String> durationString(@RequestBody TestBody body) {
+        String duration = service.getDurationString(body.getFrom(), body.getTo());
+        return ResponseEntity.ok(duration);
+    }
+
+    @PostMapping("/distance_int")
+    public ResponseEntity<Integer> distanceInt(@RequestBody TestBody body) {
+        Integer distance = service.getDistanceInt(body.getFrom(), body.getTo());
+        return ResponseEntity.ok(distance);
+    }
+
+    @PostMapping("/duration_int")
+    public ResponseEntity<Integer> durationInt(@RequestBody TestBody body) {
+        Integer duration = service.getDurationInt(body.getFrom(), body.getTo());
+        return ResponseEntity.ok(duration);
     }
 }
