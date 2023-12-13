56ef0ff0ef885b4b9cb99d1e9b28e7b31cb3dc76
bsak2 <b.sak212@gmail.com>
2023-12-13 15:46:24 +0100

    added global exception handler

---
+++ zuber/src/main/java/com/bbc/zuber/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.bbc.zuber.exceptions;
+
+import com.bbc.zuber.exceptions.dto.ValidationErrorDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private ResponseEntity<ErrorMessage> createErrorResponse(Exception ex, HttpServletRequest request, HttpStatus status) {
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .timestamp(LocalDateTime.now())
+                .code(status.value())
+                .status(status.getReasonPhrase())
+                .message(ex.getMessage())
+                .uri(request.getRequestURI())
+                .method(request.getMethod())
+                .build();
+        return new ResponseEntity<>(errorMessage, status);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> carNotFoundExceptionHandler(CarNotFoundException ex, HttpServletRequest request) {
+        return createErrorResponse(ex, request, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> driverNotFoundExceptionHandler(DriverNotFoundException ex, HttpServletRequest request) {
+        return createErrorResponse(ex, request, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> rideAssignmentNotFoundExceptionHandler(KafkaMessageProcessingException ex, HttpServletRequest request) {
+        return createErrorResponse(ex, request, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> driverNotAvailableExceptionHandler(DriversNotAvailableException ex, HttpServletRequest request) {
+        return createErrorResponse(ex, request, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> kafkaMessageProcessingExceptionHandler(KafkaMessageProcessingException ex, HttpServletRequest request) {
+        return createErrorResponse(ex, request, HttpStatus.INTERNAL_SERVER_ERROR); //INTERNAL / BAD_REQUEST ?
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<List<ValidationErrorDto>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body(
+                ex.getFieldErrors().stream()
+                        .map(
+                                fieldError -> new ValidationErrorDto(fieldError.getDefaultMessage(), fieldError.getField())
+                        ).toList());
+    }
+}
